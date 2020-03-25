f703f377172d7ae73de4bd38e485ebcb05709cd3
Xavier-alaji <xavier.gerard.mail@gmail.com>
2020-03-25 15:53:07 +0100

    index css v2

--- index.php
+++ index.php
@@ -1,17 +1,65 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>accueil</title>
-</head>
-<body>
-    <header>
-        <h1>Accueil</h1>
-        <a href="owner1.php">1 Mr Schylowski</a>
-        <a href="owner2.php">2 Mme Orlando</a>
-        <a href="owner3.php">3 Mr Petrov </a>
-        <a href="owner4.php">4 Mme Julien</a>
-    </header>
-</body>
-</html>
+.vbox 
+{
+    display: flex;
+    flex-flow: column nowrap;
+}
+
+.vbox > * 
+{
+    margin-bottom: 5%;
+}
+
+.vbox > *:last-child 
+{
+    margin-bottom: 0;
+}
+
+.hbox 
+{
+    display: flex;
+    flex-flow: row wrap;
+}
+
+.hbox > * 
+{
+    margin-right: 5%;
+}
+
+.hbox > *:last-child 
+{
+    margin-right: 0;
+}
+
+.hbox > .fluid,
+
+.vbox > .fluid 
+{
+    flex-grow: 1;
+}
+
+body 
+{
+    height: 100%;
+    margin: 0;
+}
+
+header.vbox 
+{
+    min-height: 100%;
+}
+
+header.vbox nav 
+{
+    margin: 0;
+}
+
+ul.hbox 
+{
+    background: blue;
+    border: 1px solid black;
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    justify-content: center;
+    align-items: center;
+}
