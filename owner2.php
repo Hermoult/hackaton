dc130d51816752fa9c0df0944ef9409b1db2e732
BENJYY1337 <pernot.benjamin@outlook.fr>
2020-03-25 13:33:05 +0100

    pushowner2

--- owner2.php
+++ owner2.php
@@ -3,26 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>accueil</title>
+    <title>Formulaire</title>
 </head>
 <body>
-    <h2>Connexion</h2>
-    <form action="controller.php" method="GET" id="form">
-        <div class="name">
-            <label for="name">name</label> 
-            <input type="text" name="name" id="name" placeholder="Nom">
-        </div>
-        <div class="password">
-            <label for="password">password</label>
-            <input type="password" name="password" id="password" placeholder="******">
-        </div>
-        <div class="identification">
-            <label for="identification">Numéro identification</label> 
-            <input type="text" name="identification" id="identification" placeholder="Rentrez id">
-        </div>
-        <div class="submit">
-            <input type="submit">
-        </div>
-    </form>    
+    <form action="controller.php" method="POST">
+        <fieldset>
+            <legend>Formulaire</legend>
+            <div>
+                <label for="nameinput">name</label>
+                <input type="text" name="name" id="nameinput">
+            </div>
+            <div>
+                <label for="idinput">identification</label>
+                <input type="text" name="identification" id="idinput">
+            </div>
+            <div>
+                <label for="passwordinput">password</label>
+                <input type="password" name="password" id="passwordinput">
+            </div>
+            <div>
+                <label><input type="checkbox" name="cgu" required checked>J'accepte les conditions d'utilisations</label>
+            </div>
+            <input type="submit" value="Envoyer">
+            <input type="reset" value="Annuler">
+        </fieldset>
+    </form>
 </body>
 </html>
