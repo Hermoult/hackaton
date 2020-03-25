6b686b8095096e7150e36b8eaa2b55f36014e172
Hermoult <adrienhergat@gmail.com>
2020-03-25 11:27:05 +0100

    créations des liens

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>accueil</title>
+</head>
+<body>
+    <header>
+        <h1>Mon CRUD</h1>
+        <a href="owner1.php.">1 Mr Schylowski</a>
+        <a href="owner2.php.">2 Mme Orlando</a>
+        <a href="owner3.php.">3 Mr Petrov </a>
+        <a href="owner4.php.">4 Mme Julien</a>
+    </header>
+</body>
+</html>
